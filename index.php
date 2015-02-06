2cf4a84a5247f698b5f01cc4c78cd08e821ca2cf
Yaroslav Horbunov <yaroslav_gorbunov@mail.ru>
2015-02-06 17:02:27 +0200

    Menus FirstCommit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LeftMenu</title>
+<meta charset="utf-8">
+<link href="./css/leftmenu_ss.css" rel="stylesheet"/>
+<link href="./css/leftmenu_fcs.css" rel="stylesheet">
+</head>
+
+<body>
+
+<ul class="leftmenu_ss">
+	<li class="home">
+		<a>Haus</a>
+	</li>
+	<li class="programm">
+		<a>Programm</a>
+	</li>
+	<li class="news">
+		<a>Die Neuheiten</a>
+	</li>
+	<li class="information unfolding">
+		<a>Die Information</a>
+		<a>Den Wert</a>
+		<a>Das Stipendium</a>
+		<a>Den Wohnraum</a>
+	</li>
+	<li class="contacts">
+		<a>Die Kontakte</a>
+	</li>
+	<li class="location">
+		<a>Die Lage</a>
+	</li>
+</ul>
+
+<ul class="leftmenu_fcs">
+	<li class="main">
+		<a>Главная</a>
+	</li>
+	<li class="about_fcs unfolding_fcs">
+		<a>О факультете</a>
+		<a>Абитуриентам</a>
+		<a>Кафедра</a>
+		<a>Наука</a>
+	</li>
+	<li class="contacts_fcs">
+		<a>Контакты</a>
+	</li>
+	<li class="links">
+		<a>Полезные ссылки</a>
+	</li>
+</ul>
+
+<ul class="leftmenu_fcs_v2">
+	<li><a href="#">Главная</a></li>
+	<li class="unfolding_fcs_v2"><a href="#">О факультете</a>
+		<ul class="context_menu">
+     		<li><a href="#">Абитуриентам</a></li>
+     		<li><a href="#">Кафедра</a></li>
+     		<li><a href="#">Наука</a></li>
+       </ul>
+    </li>
+    <li><a href="#">Контакты</a>
+    <li><a href="#">Полезные ссылки</a>
+</ul>
+
+</body>
+</html>
